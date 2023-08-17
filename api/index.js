99c390b5ff980ac8d152970e0aed0042ad3d978b
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-08-16 20:05:37 -0400

    feat: add dependencie bodyParser and Faker

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const config = require('../config/config');
+const bodyParser = require('body-parser');
 
 const post = require('./components/posts/network')
 
 const port = config.app.port;
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(`${config.app.prefix}/posts`, post)
 
